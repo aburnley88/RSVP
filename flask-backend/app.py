9cb124183b650bb26a5a489ba6c3003cbde91642
Arnold Burnley <aburnleylvx@outlook.com>
2023-10-29 12:53:20 -0400

    added event deets

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -35,4 +35,4 @@ def get_rsvps():
     return jsonify([{'name': rsvp.name, 'email': rsvp.email} for rsvp in rsvps]), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
